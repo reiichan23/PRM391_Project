a089bf932180ff4de93283b019d2ce01b1aedc77
tuanFMV2276 <tuanbqqe170037@fpt.edu.vn>
2024-10-18 15:51:51 +0700

    create BaseDomain

---
+++ app/src/main/java/Activity/BaseActivity.java
@@ -0,0 +1,17 @@
+package Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.prm391_project.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class BaseActivity extends AppCompatActivity {
+FirebaseAuth mAuth;
+FirebaseDatabase database;
+    public String TAG="Group2";
+
+}
